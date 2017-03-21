2e9193a6987f1d8e7cb381583ac571f3add2eafc
Shinsuke Nishio <nishio@densan-labs.net>
2017-03-22 01:44:02 +0900

    implements getObjectHandler

--- s3handlers/get-object.go
+++ s3handlers/get-object.go
@@ -1,9 +1,14 @@
 package s3handlers
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	s3sdk "github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gorilla/mux"
 	"net/http"
 	"the-interceptor/api"
+	"the-interceptor/db"
+	"the-interceptor/s3client"
+	"io/ioutil"
 )
 
 /**
@@ -16,11 +21,49 @@ func GetObjectHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: Support 403 Forbidden (Authorization)
 	// TODO: Support Not Found
 	v := mux.Vars(r)
-	_, err := GetInterceptorBucket(v["bucket"])
+	bucket, err := GetInterceptorBucket(v["bucket"])
 	if err != nil {
 		SendNoSuchBucketError(v["bucket"], w, r)
 		return
 	}
+	key := v["object"]
+	readBucket := bucket.GetReadBucket()
+	writeBucket := bucket.GetWriteBucket()
 
-	api.SendSuccess(w, []byte("Call2"), "Test")
+	ro, _ := getObject(readBucket, key)
+	wo, _ := getObject(writeBucket, key)
+
+	// FIXME: Need Refactor
+	if ro != nil {
+		defer ro.Body.Close()
+		b, e := ioutil.ReadAll(ro.Body)
+		if e != nil {
+			SendInternalError("Something Happend", w, r)
+		} else {
+			api.SendSuccess(w, b, *ro.ContentType)
+		}
+	} else if wo != nil {
+		defer wo.Body.Close() // TODO: fix memory leak if ro/wo is present
+		b, e := ioutil.ReadAll(wo.Body)
+		if e != nil {
+			SendInternalError("Something Happend", w, r)
+		} else {
+			api.SendSuccess(w, b, *wo.ContentType)
+		}
+	} else {
+		SendInternalError("404 NotFound. Not Implemented Yet", w, r)
+	}
+}
+
+func getObject(bucket *db.S3Bucket, key string) (*s3sdk.GetObjectOutput, error) {
+	// TODO: Support range Get
+	client := s3client.GetS3Client(bucket)
+	resp, err := client.GetObject(&s3sdk.GetObjectInput{
+		Bucket: aws.String(bucket.BucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
 }
